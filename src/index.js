c517f9b4f9f850190e56791d706605fc0bac69c9
BethJW <bethanyjwilliamson16@gmail.com>
2023-12-13 13:28:51 +0000

    added the AI Api and added axios

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ recipeForm.addEventListener("submit", generate);
 
 function generate(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let apiKey = `b9b10f3af19o1ba6ec0aed0664cb453t`;
+  let prompt = `Please generate a recipe based on ${searchInput.value}`;
+  let context = `You are a highly skilled AI Baking assistant that knows a range of different dessert recipes from just one ingredient.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayRecipe);
+  let recipeContainer = document.querySelector(`#recipe`);
+  recipeContainer.innerHTML = `Please wait a moment...`;
+}
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "cupcake are so good",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
   });
